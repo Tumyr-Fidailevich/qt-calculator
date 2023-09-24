e1d43729b5ed8b31d6ac804989e625cc724a485f
Timur <timurkashapov15@gmail.com>
2023-09-24 21:06:08 +0300

    added slots and parsseInput function

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,9 +1,7 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 }
@@ -13,3 +11,92 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::add0()
+{
+
+}
+
+void MainWindow::add1()
+{
+
+}
+
+void MainWindow::add2()
+{
+
+}
+
+void MainWindow::add3()
+{
+
+}
+
+void MainWindow::add4()
+{
+
+}
+
+void MainWindow::add5()
+{
+
+}
+
+void MainWindow::add6()
+{
+
+}
+
+void MainWindow::add7()
+{
+
+}
+
+void MainWindow::add8()
+{
+
+}
+
+void MainWindow::add9()
+{
+
+}
+
+void MainWindow::add_dot()
+{
+
+}
+
+void MainWindow::add_derivative()
+{
+
+}
+
+void MainWindow::add_multiplication()
+{
+
+}
+
+void MainWindow::add_minus()
+{
+
+}
+
+void MainWindow::add_plus()
+{
+
+}
+
+void MainWindow::deletion()
+{
+
+}
+
+void MainWindow::evaluate()
+{
+
+}
+
+std::string MainWindow::parseInput()
+{
+    return "";
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <string>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,5 +18,29 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    bool _evaluated = false;
+    bool _operation = false;
+    bool _dot = false;
+    char _currentSymbol = 0;
+    std::string parseInput();
+public slots:
+    void add0();
+    void add1();
+    void add2();
+    void add3();
+    void add4();
+    void add5();
+    void add6();
+    void add7();
+    void add8();
+    void add9();
+    void add_dot();
+    void add_derivative();
+    void add_multiplication();
+    void add_minus();
+    void add_plus();
+    void deletion();
+    void evaluate();
+
 };
 #endif // MAINWINDOW_H
